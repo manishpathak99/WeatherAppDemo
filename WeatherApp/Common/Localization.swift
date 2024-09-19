fbedc61f861612d5bae0c990d1a303b82efe7c3a
Manish Pathak <pathak.ims@gmail.com>
2024-09-19 12:08:29 +0530

    Added generic localisation file

---
+++ WeatherApp/Common/Localization.swift
@@ -0,0 +1,19 @@
+//
+//  Localization.swift
+//  WeatherApp
+//
+//  Created by MANISH PATHAK
+//
+
+import Foundation
+
+struct Localization {
+
+    static let somethingWentWrong = NSLocalizedString("something_went_wrong", comment: "")
+    static let error = NSLocalizedString("error", comment: "")
+    static let ok = NSLocalizedString("ok", comment: "")
+    static let weatherApp = NSLocalizedString("weather_app", comment: "")
+    static let noInternetConnection = NSLocalizedString("no_internet_connection", comment: "")
+    static let checkInternetConnection = NSLocalizedString("check_internet_connection", comment: "")
+    static let noMoreDataAvailable = NSLocalizedString("no_more_data_available", comment: "")
+}
